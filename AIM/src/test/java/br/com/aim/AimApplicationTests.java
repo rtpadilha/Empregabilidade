f737775679f440018572564192d56474b4646fd1
rtpadilha <66036266+rtpadilha@users.noreply.github.com>
2020-10-11 01:17:06 -0300

    Inicio

---
+++ AIM/src/test/java/br/com/aim/AimApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
